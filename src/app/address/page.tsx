bf8764b0b5519881f22c15b5e1b4bec63bf84f07
mrahadmehar <mrabidmehar890@gmail.com>
2024-09-29 06:52:21 +0500

    Footer-Project

--- src/app/address/page.tsx
+++ src/app/address/page.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 const page = () => {
   return (
-    <div><figure className="md:flex bg-gray-600 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/home1.jpg" alt="" width="384" height="512"/>
+    <div><figure className="md:flex bg-pink-300 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/about1.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        My home is about 6marla and consist of 4 story.There are 6 rooms and 2 kichan in it.There is a park in the front of my home.
+        House no.56,Saeed Tone,Rahim yar Khan,Zip code 64200,Punjab.
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-         House Location.
+          Address
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          Saeed Tona/RYK.
+          RYK.Punjab.
         </div>
       </figcaption>
     </div>
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,20 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 const page = () => {
   return (
-    <div><figure className="md:flex bg-gray-600 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/home1.jpg" alt="" width="384" height="512"/>
+    <div><figure className="md:flex bg-green-500 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <Image className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/contact1.jpg" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
-        My home is about 6marla and consist of 4 story.There are 6 rooms and 2 kichan in it.There is a park in the front of my home.
+        Phone no.03282346728 Email address:mrahadmehar5011@gmail.com.
         </p>
       </blockquote>
       <figcaption className="font-medium">
         <div className="text-sky-500 dark:text-sky-400">
-         House Location.
+          contact
         </div>
         <div className="text-slate-700 dark:text-slate-500">
-          Saeed Tona/RYK.
+          +92 3282346728.
         </div>
       </figcaption>
     </div>
